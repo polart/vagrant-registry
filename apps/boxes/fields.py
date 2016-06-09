ef4f3ecf01ce31e1a2e0427ebf86b86221b9a2bf
Artem Polunin <Artem.Pun@gmail.com>
2016-06-09 22:57:51 +0300

    Create hyperlinked serializer fields with multiple lookup fields

--- apps/boxes/fields.py
+++ apps/boxes/fields.py
@@ -1,8 +1,13 @@
+from rest_framework.fields import get_attribute
 from rest_framework.relations import HyperlinkedIdentityField, HyperlinkedRelatedField
 
 
-class BoxHyperlinkedMixin:
-    """ Mixin for generating URL for Box instance with multiple lookup fields. """
+class MultiLookupHyperlinkedMixin:
+    """ Mixin for generating URL for Box instance with multiple lookup fields.
+
+    Multiple lookup fields should be in form of dictionary with fields as keys
+    and URL kwarg attributes as values.
+    """
 
     def get_url(self, obj, view_name, request, format):
         """ Given an object, return the URL that hyperlinks to the object.
@@ -14,24 +19,33 @@ class BoxHyperlinkedMixin:
         if hasattr(obj, 'pk') and obj.pk in (None, ''):
             return None
 
+        kwargs = {}
+        for field, kwarg in self.multi_lookup_map.items():
+            kwargs[kwarg] = get_attribute(obj, field.split('.'))
+
         return self.reverse(
             view_name,
-            kwargs={
-                'owner__username': obj.owner.username,
-                'name': obj.name
-            },
+            kwargs=kwargs,
             request=request,
             format=format
         )
 
 
-class BoxHyperlinkedIdentityField(BoxHyperlinkedMixin, HyperlinkedIdentityField):
+class MultiLookupHyperlinkedIdentityField(MultiLookupHyperlinkedMixin,
+                                          HyperlinkedIdentityField):
     """
     A read-only field that represents the identity URL for an object, itself.
     """
-    pass
+    def __init__(self, view_name=None, **kwargs):
+        self.multi_lookup_map = kwargs.pop(
+            'multi_lookup_map', {self.lookup_field, self.lookup_field})
+        super().__init__(view_name=view_name, **kwargs)
 
 
-class BoxHyperlinkedRelatedField(BoxHyperlinkedMixin, HyperlinkedRelatedField):
+class MultiLookupHyperlinkedRelatedField(MultiLookupHyperlinkedMixin,
+                                         HyperlinkedRelatedField):
     """ A field that represents the URL of relationships to Box object. """
-    pass
+    def __init__(self, view_name=None, **kwargs):
+        self.multi_lookup_map = kwargs.pop(
+            'multi_lookup_map', {self.lookup_field, self.lookup_field})
+        super().__init__(view_name=view_name, **kwargs)
