45369d4ef56f6b068601a744a8a8c72bc7dfff04
Artem Polunin <Artem.Pun@gmail.com>
2016-09-13 20:45:52 +0300

    User PostgreSQL instead of SQLite

--- vagrant_registry/settings/base.py
+++ vagrant_registry/settings/base.py
@@ -81,8 +81,12 @@ AUTHENTICATION_BACKENDS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vagrant_registry',
+        'USER': 'vagrant_registry',
+        'PASSWORD': 'vagrant_registry',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
