854df94062bd0d8080df3709ab558f1931a5795c
Artem Polunin <Artem.Pun@gmail.com>
2016-09-20 21:26:07 +0300

    Add production settings

---
+++ vagrant_registry/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'vagrant_registry'),
+        'USER': os.environ.get('DB_USER', 'vagrant_registry'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'vagrant_registry'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+X_FRAME_OPTIONS = 'DENY'
